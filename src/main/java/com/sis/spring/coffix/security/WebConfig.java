8282ac7a8dc09cc988177a723d74c34adec790b1
daniel69zz <luisdanielrojascaceres@gmail.com>
2025-12-11 07:42:13 -0400

    all origins

--- src/main/java/com/sis/spring/coffix/security/WebConfig.java
+++ src/main/java/com/sis/spring/coffix/security/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") 
-                .allowedOrigins("http://localhost:5173") // tu frontend
+                .allowedOrigins("*") // frontend
                 .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true); 
